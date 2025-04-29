52c4e96a20031610b38e0f02e2333d317ab87cd7
Joaquim <jocaq124@gmail.com>
2025-04-29 11:46:54 -0300

    Alterado model, e controller de jogos, para adicionar, e remover desconto

--- APIDevSteam/Migrations/20250429143013_Jogos12345.Designer.cs
+++ APIDevSteam/Migrations/20250429143013_Jogos12345.Designer.cs
@@ -4,6 +4,7 @@ using APIDevSteam.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace APIDevSteam.Migrations
 {
     [DbContext(typeof(ApiDevsteamContext))]
-    partial class ApiDevsteamContextModelSnapshot : ModelSnapshot
+    [Migration("20250429143013_Jogos12345")]
+    partial class Jogos12345
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- APIDevSteam/Models/Jogo.cs
+++ APIDevSteam/Models/Jogo.cs
@@ -8,7 +8,7 @@
         public int Desconto { get; set; }
         public string Banner { get; set; }
         public string Descricao { get; set; }
-
+        public decimal? PrecoOriginal { get; set; }
 
     }
 }
